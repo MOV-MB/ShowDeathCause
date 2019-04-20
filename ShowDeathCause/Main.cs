00200ecd660dfd84c1137ce13b2fa24ebadc831d
Storm312 <31706230+Storm312@users.noreply.github.com>
2019-04-20 23:26:58 +0200

    Add files via upload

---
+++ ShowDeathCause/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BepInEx;
+using RoR2;
+
+namespace ShowDeathCause
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.MyCompanyName.MyModName", "ShowDeathCause", "1.0.0")]
+    public class MyModName : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+
+        }
+    }
+}
